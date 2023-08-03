bb4c101b5a5d56db4ae03381b4370da0140b77ba
李文韬 <2443912540@qq.com>
2023-08-03 11:54:14 +0800

    Add:蓝桥杯学习材料/每日一题230730~230802

---
+++ LanQiao/SummerCamp_Basis/Chapter4 Dichotomous and bipointing/4.7.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const int N=2e5+10;
+ll n,k,m;
+ll a[N];
+vector<ll> b;
+
+int main()
+{
+    cin>>n>>k>>m;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    int l=1,
+}
